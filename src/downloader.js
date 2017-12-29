f22dab77eecaa095dbbb5fa2aca5eca11a5be659
Balaganesh Damodaran <asleepysamurai@gmail.com>
2017-12-29 15:24:09 +0530

    Initial Commit. Basic resumable download

---
+++ src/downloader.js
@@ -0,0 +1,100 @@
+/**
+ * Given a url and a directory, 
+ * downloads the tar file at that url
+ * Once downloaded untars the tar file
+ * into specified directory.
+ *
+ * Can resume downloads.
+ */
+
+const argv = require('minimist')(process.argv.slice(2));
+const debug = require('debug')('au');
+const mkdirp = require('make-dir');
+const path = require('path');
+const mtd = require('zeltice-mt-downloader');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const access = promisify(fs.access);
+const unlink = promisify(fs.unlink);
+
+function messageAndExit(message, success = true) {
+    message.success = success;
+
+    if (process.send)
+        process.send(message);
+    else
+        debug(message);
+    process.exit(success ? 0 : 1);
+};
+
+function fileExists(filePath) {
+    return access(filePath, fs.constants.F_OK);
+};
+
+function fileReadWritable(filePath) {
+    return access(filePath, fs.constants.R_OK | fs.constants.W_OK);
+};
+
+function download(fileOrPath, url, onStart = () => {}, onEnd = () => {}) {
+    debug(`Downloading with url: ${url} and filePath: ${fileOrPath}`);
+    const downloader = new mtd(fileOrPath, url, {
+        onStart,
+        onEnd
+    });
+
+    downloader.start();
+};
+
+async function startOrResumeDownload(url, dir, fileName) {
+    const downloadFileName = fileName || path.basename(url);
+
+    const downloadFilePath = path.resolve(dir, downloadFileName);
+    const resumeFilePath = path.resolve(dir, `${downloadFileName}.mtd`);
+
+    function onStart() {
+        debug(`Starting download of ${url} to ${resumeFilePath}`);
+    };
+
+    async function onEnd(err) {
+        if (err && err.message && err.message.indexOf('.mtd') > -1) {
+            debug(`.mtd corrupt. delete and start again`);
+            await unlink(resumeFilePath);
+            return await startOrResumeDownload(url, dir);
+        }
+        debug(`Finished download of ${url} with ${err}`);
+    };
+
+    try {
+        await fileExists(resumeFilePath);
+    } catch (err) {
+        return await download(downloadFilePath, url, onStart, onEnd);
+    }
+
+    try {
+        await fileReadWritable(resumeFilePath);
+    } catch (err) {
+        debug(`Cannot read/write ${resumeFilePath}. Creating anew.`);
+        return startOrResumeDownload(url, dir, `${downloadFileName}-1`);
+    }
+
+    await download(resumeFilePath, null, onStart, onEnd);
+};
+
+async function init() {
+    const { url, dir } = argv;
+
+    if (!(url && dir))
+        return messageAndExit({ code: 'EBADPARAMS' });
+
+    const tempDirPath = path.join(dir, ' ./temp');
+    try {
+        await mkdirp(tempDirPath);
+    } catch (err) {
+        return messageAndExit({ code: 'EMAKEDIRFAIL' });
+    }
+
+    await startOrResumeDownload(url, dir);
+};
+
+init();
---
+++ src/index.js
@@ -0,0 +1,58 @@
+/**
+ * AutoUpdater Entry Point
+ */
+
+const path = require('path');
+const fork = require('child_process').fork();
+const superagent = require('superagent');
+
+function spawnDownloader(url, dir, onUpdateReady, onUpdateFail) {
+    return new Promise((resolve, reject) => {
+        const cp = fork(path.join(__dirname, './downloader'), [`--url=${url}`, `--dir=${dir}`], { execArgv: [`--inspect=${++lastDebugPort}`] });
+
+        cp.once('message', message => {
+            if (message.success && message.code == 'DOWNLOADSTARTED') {
+                cp.once('message', message => {
+                    if (message.success && message.code == 'DOWNLOADENDED')
+                        return onUpdateReady();
+
+                    return onUpdateFail(message);
+                });
+
+                return resolve();
+            }
+
+            return reject(message);
+        });
+    });
+};
+
+async function updateIfAvailable(opts) {
+    if (!(opts && opts.url && opts.shouldDownload && opts.getDownloadURL))
+        throw new Error('Invalid update check url');
+
+    let updateJSON;
+    try {
+        const update = await superagent.get(opts.url);
+        updateJSON = JSON.parse(update);
+    } catch (err) {
+        if (err instanceof SyntaxError) {
+            const error = new Error('Invalid update manifest');
+            error.code == 'EBADMANIFEST';
+            throw error;
+        } else {
+            const error = new Error('Update check failed');
+            error.code == 'ECHECKFAIL';
+            throw error;
+        }
+    }
+
+    const shouldDownload = opts.shouldDownload(updateJSON);
+    if (shouldDownload instanceof Promise ? await shouldDownload : shouldDownload) {
+        return await spawnDownloader(opts.getDownloadURL(updateJSON));
+    }
+};
+
+module.exports = {
+    updateIfAvailable
+};
